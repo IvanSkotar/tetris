329c3b868d15289820069b95c48b13deade37f7a
Ivan Skotar <iskotar.it@gmail.com>
2021-05-09 00:28:29 -0700

    Version 1.2

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('canv');
+const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const row = 24;
 const col = 10;
@@ -8,15 +8,17 @@ let currentScore = 0;
 let speed = 800;
 let level = 1;
 let game;
-document.getElementById('score').value = currentScore;
-document.getElementById('level').innerText = `Level ${level}`;
+const displayScore = (score) => document.getElementById('score').innerText = `Score ${score}`;
+const setLevel = (level) => document.getElementById('level').innerText = `Level ${level}`;
+displayScore(currentScore);
+setLevel(level);
 
 function startNewGame(){
   prevScore = 0;
   currentScore = 0;
   speed = 800;
   level = 1;
-  document.getElementById('level').innerText = `Level ${level}`;
+  setLevel(level);
   clearInterval(game);
   newGameCircle();
 }
@@ -144,32 +146,7 @@ async function newGameCircle () {
   ];
 
 //The game board in array
-  let landed = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+  let landed = Array.from(Array(24), x => Array.from(Array(10), y => 0));
   let posY = 2;
   let posX = 4;
   let rotatePos = 0;
@@ -179,7 +156,7 @@ async function newGameCircle () {
     context.fillStyle = color; // Set color for square field
     context.fillRect(x * SQ, y * SQ, SQ, SQ); //Draw a field color
 
-    context.strokeStyle = "WHITE"; // Set field grid color
+    context.strokeStyle = "#f0f0f0"; // Set field grid color
     context.strokeRect(x * SQ, y * SQ, SQ, SQ); //Draw a field grid
   }
 
@@ -197,13 +174,8 @@ async function newGameCircle () {
   function drawBoard () {
     for (let r in landed) {
       for (let c in landed[r]) {
-        if (landed[r][c] === 0) {
-          drawField(c, r, "WHITE");
-          console.log('===redrowed to white===')
-        } else {
-          drawField(c, r, "BLUE");
-          console.log('===redrowed to blue===')
-        }
+        if (landed[r][c] === 0) drawField(c, r, "WHITE");
+        else drawField(c, r, "BLUE");
       }
     }
   }
@@ -219,10 +191,10 @@ async function newGameCircle () {
 
 //Draw tetro figure on board
   function drawTetro (tetro, color) {
-    drawField(posX, posY, color);
-    drawField(posX + tetro[0][0], posY + tetro[0][1], color);
-    drawField(posX + tetro[1][0], posY + tetro[1][1], color);
-    drawField(posX + tetro[2][0], posY + tetro[2][1], color);
+    for(let i = 0; i <= 3; i++){
+      if(i === 3) drawField(posX, posY, color);
+      else drawField(posX + tetro[i][0], posY + tetro[i][1], color);
+    }
   }
 
   drawTetro(randomTetro[0], "BLUE");
@@ -269,7 +241,8 @@ async function newGameCircle () {
       drawTetro(randomTetro[rotatePos], "WHITE");
       rotatePos = 0;
       drawTetro(randomTetro[rotatePos], "BLUE");
-    } else if (
+    }
+    else if (
       posX + 1 > col - 1 || posX + 1 < 0 ||
       randomTetro[rotatePos + 1][0][0] + posX > col - 1 || randomTetro[rotatePos + 1][0][0] + posX < 0 ||
       randomTetro[rotatePos + 1][1][0] + posX > col - 1 || randomTetro[rotatePos + 1][1][0] + posX < 0 ||
@@ -277,12 +250,9 @@ async function newGameCircle () {
       landed[posY][posX + 1] === 1 ||
       landed[randomTetro[rotatePos][0][1] + posY][randomTetro[rotatePos][0][0] + posX + 1] === 1 ||
       landed[randomTetro[rotatePos][1][1] + posY][randomTetro[rotatePos][1][0] + posX + 1] === 1 ||
-      landed[randomTetro[rotatePos][2][1] + posY][randomTetro[rotatePos][2][0] + posX + 1] === 1 ||
-      landed[posY][posX - 1] === 1 ||
-      landed[randomTetro[rotatePos][0][1] + posY][randomTetro[rotatePos][0][0] + posX - 1] === 1 ||
-      landed[randomTetro[rotatePos][1][1] + posY][randomTetro[rotatePos][1][0] + posX - 1] === 1 ||
-      landed[randomTetro[rotatePos][2][1] + posY][randomTetro[rotatePos][2][0] + posX - 1] === 1
-    ) {} else {
+      landed[randomTetro[rotatePos][2][1] + posY][randomTetro[rotatePos][2][0] + posX + 1] === 1
+    ) {}
+    else {
       drawTetro(randomTetro[rotatePos], "WHITE");
       rotatePos++;
       drawTetro(randomTetro[rotatePos], "BLUE");
@@ -299,16 +269,16 @@ async function newGameCircle () {
       landed[randomTetro[rotatePos][1][1] + posY + 1][randomTetro[rotatePos][1][0] + posX] === 1 ||
       landed[randomTetro[rotatePos][2][1] + posY + 1][randomTetro[rotatePos][2][0] + posX] === 1
     ) {
-      landed[posY][posX] = 1;
-      landed[randomTetro[rotatePos][0][1] + posY][randomTetro[rotatePos][0][0] + posX] = 1;
-      landed[randomTetro[rotatePos][1][1] + posY][randomTetro[rotatePos][1][0] + posX] = 1;
-      landed[randomTetro[rotatePos][2][1] + posY][randomTetro[rotatePos][2][0] + posX] = 1;
+      for(let i = 0; i <= 3; i++){
+        if(i === 3) landed[posY][posX] = 1;
+        else landed[randomTetro[rotatePos][i][1] + posY][randomTetro[rotatePos][i][0] + posX] = 1;
+      }
       for (let r in landed) {
         if (landed[r].reduce((a,b) => a + b) === 10) {
           landed.splice(+r, 1);
           landed.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
           prevScore = currentScore;
-          currentScore += 10;
+          currentScore += 100;
           speedUp();
         }
       }
@@ -322,23 +292,30 @@ async function newGameCircle () {
       posY++;
       drawTetro(randomTetro[rotatePos], "BLUE");
     }
-    document.getElementById('score').value = currentScore;
+    displayScore(currentScore);
   }
 
   //Move tetro figures by arrow keys
   function moveTetro (e) {
-    if (e.keyCode === 40) {
-      fallingTetro();
-    }
-    if (e.keyCode === 39) {
-      right();
-    }
-    if (e.keyCode === 37) {
-      left();
-    }
-    if (e.keyCode === 38) {
-      rotate();
-    }
+    if (e.keyCode === 40) fallingTetro();
+    if (e.keyCode === 39) right();
+    if (e.keyCode === 37) left();
+    if (e.keyCode === 38) rotate();
+  }
+
+  let touochStart = {};
+  function tSt(e) {
+    touochStart.x = e.touches[0].clientX;
+    touochStart.y = e.touches[0].clientY;
+  }
+  document.addEventListener('touchstart', tSt, false);
+  document.addEventListener('touchmove', onMouseUpdate, false);
+  document.addEventListener('mousedown', this.rotate, false);
+
+  function onMouseUpdate(e){
+    if(e.changedTouches[0].clientX > touochStart.x && Math.trunc(e.changedTouches[0].clientX) / 10 % 2 === 0) right();
+    if(e.changedTouches[0].clientX < touochStart.x && Math.trunc(e.changedTouches[0].clientX) / 10 % 2 === 0) left();
+    if(e.changedTouches[0].clientY > touochStart.y && Math.trunc(e.changedTouches[0].clientY) / 10 % 2 === 0) fallingTetro();
   }
 
   document.onkeydown = moveTetro;
@@ -360,62 +337,62 @@ async function newGameCircle () {
         newGameCircle();
         speed = 700;
         level = 2;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         break;
       case prevScore < 200 && currentScore >= 200:
         clearInterval(game);
         speed = 600;
         level = 3;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       case prevScore < 300 && currentScore >= 300:
         clearInterval(game);
         speed = 500;
         level = 4;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       case prevScore < 400 && currentScore >= 400:
         clearInterval(game);
         speed = 400;
         level = 5;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       case prevScore < 500 && currentScore >= 500:
         clearInterval(game);
         speed = 300;
         level = 6;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       case prevScore < 600 && currentScore >= 600:
         clearInterval(game);
         speed = 250;
         level = 7;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       case prevScore < 700 && currentScore >= 700:
         clearInterval(game);
         speed = 200;
         level = 8;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       case prevScore < 800 && currentScore >= 800:
         clearInterval(game);
         speed = 150;
         level = 9;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       case prevScore < 900 && currentScore >= 900:
         clearInterval(game);
         speed = 100;
         level = 10;
-        document.getElementById('level').innerText = `Level ${level}`;
+        setLevel(level);
         newGameCircle();
         break;
       default:
